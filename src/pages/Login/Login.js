fcd6670aa8e78f3ec4634e33f1f8fb2d15142cad
Joe Arlo Baltazar <>
2023-05-23 19:06:45 +0800

    work in progress cors issue

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,11 +8,12 @@ import {
   Image,
   Row
 } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { illustration, logo } from '../../assets/assets'
+// import axios from 'axios'
 
 const Login = () => {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const [formData, setFormData] = useState({
     username: '',
@@ -22,32 +23,69 @@ const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault()
     try {
-      const formdata = new FormData()
-      formdata.append('grant_type', 'password')
-      formdata.append('client_id', '991776b6-5d85-4149-91a5-5a627e247c00')
-      formdata.append('client_secret', 'yhe2Uzjj4SI3t8KRPxQIqtVnu8VEiKC3FOmU26Kx')
-      formdata.append('username', formData.username)
-      formdata.append('password', formData.password)
-      formdata.append('scope', '')
+      // const formdata = new FormData()
+      // formdata.append('grant_type', 'password')
+      // formdata.append('client_id', '992f56da-abc9-4fb1-8ff2-5466e6e0c33f')
+      // formdata.append('client_secret', 'Q6cMnm9EVzPb5abG7Jh8iXHM1AW4GwLIS9dJyVjK')
+      // formdata.append('username', formData.username)
+      // formdata.append('password', formData.password)
+      // formdata.append('scope', '')
 
-      const requestOptions = {
+      // const formData = {
+      //   grant_type: 'password',
+      //   client_id: '992f56da-abc9-4fb1-8ff2-5466e6e0c33f',
+      //   client_secret: 'Q6cMnm9EVzPb5abG7Jh8iXHM1AW4GwLIS9dJyVjK',
+      //   username: 'employee@sample.com',
+      //   password: 'sample'
+      // }
+
+      // console.log(formData)
+
+      // const requestOptions = {
+      //   method: 'POST',
+      //   body: {},
+      //   redirect: 'follow',
+      //   mode: 'no-cors'
+      // }
+
+      const response = await fetch('https://dunhilldale.com/bench-ease/public/oauth/token', {
         method: 'POST',
-        body: formdata,
-        redirect: 'follow',
+        body: {
+          grant_type: 'password',
+          client_id: '98fb8a9e-b05e-40e0-a61d-cd478a5709eb',
+          client_secret: 'LhNsTIrsL3tJcDM141ihAHTogQpc7gYOc5kjYrUg',
+          username: 'admin@sample.com',
+          password: 'sample',
+          scope: ''
+        },
         mode: 'no-cors'
-      }
+      })
 
-      const response = await fetch('https://brm.kierquebral.com/oauth/token', requestOptions)
-      if (!response.ok) {
-        throw new Error('Authentication failed')
-      }
+      console.log(response)
+      // const response = await fetch('https://brm.kierquebral.com/oauth/token', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-type': 'application/json; charset=UTF-8'
+      //   },
+      //   redirect: 'follow',
+      //   body: formData,
+      //   mode: 'no-cors'
+      // })
+
+      // if (response.status === 422 || response.status === 401) {
+      //   return response
+      // }
+
+      // if (!response.ok) {
+      //   throw new Error('Authentication failed')
+      // }
 
       // eslint-disable-next-line camelcase
-      const { access_token } = await response.json()
-      localStorage.setItem('token', access_token)
-      navigate('/')
+      // const { access_token } = await response.json()
+      // localStorage.setItem('token', access_token)
+      // navigate('/')
     } catch (error) {
-      console.error(error)
+      console.log(error.message)
     }
   }
 
